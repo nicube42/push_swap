407d246a42615452ae859f56b8486a52fbe161ad
nicube42 <129955928+nicube42@users.noreply.github.com>
2023-04-13 21:55:12 +0200

    small cleanup 5 numbers

--- srcs/small_stack.c
+++ srcs/small_stack.c
@@ -6,7 +6,7 @@
 /*   By: nicolasdiamantis <nicolasdiamantis@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 13:51:26 by ndiamant          #+#    #+#             */
-/*   Updated: 2023/04/13 21:40:59 by nicolasdiam      ###   ########.fr       */
+/*   Updated: 2023/04/13 21:54:05 by nicolasdiam      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,18 @@ void	ft_four_numbers(t_first *first_a, t_first *first_b)
 	stack_b = first_b->first;
 	ft_push(first_a, first_b, 1);
 	ft_three_numbers(first_a, 0);
+	ft_push_back(first_a, first_b);
+	if (first_a->first->content < first_b->first->content)
+		ft_rotate(first_a, 0);
+	ft_push(first_b, first_a, 0);
+	ft_rotate(first_a, 0);
+}
+
+void	ft_push_back(t_first *first_a, t_first *first_b)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+
 	stack_a = first_a->first;
 	stack_b = first_b->first;
 	while (stack_a != NULL)
@@ -60,7 +72,7 @@ void	ft_four_numbers(t_first *first_a, t_first *first_b)
 		ft_index(first_a);
 		if (first_a->first->content < first_b->first->content)
 		{
-			if (stack_b->index >= 2)
+			if (stack_b->index <= 2)
 			{
 				while (first_a->first->content != ft_is_biggest(first_a))
 					ft_rotate(first_a, 0);
@@ -73,23 +85,20 @@ void	ft_four_numbers(t_first *first_a, t_first *first_b)
 		}
 		else
 		{
-			if (stack_b->index >= 2)
+			if (stack_b->index <= 2)
 			{
-				while (first_a->first->index != 1)
+				while (first_a->first->content != ft_is_smallest(first_a))
 					ft_rotate(first_a, 0);
 			}
 			else
 			{
-				while (first_a->first->index != 1)
+				while (first_a->first->content != ft_is_smallest(first_a))
 					ft_reverse_rotate(first_a, 0);
 			}
 		}
 		stack_a = stack_a->next;
 	}
-	if (first_a->first->content < first_b->first->content)
-		ft_rotate(first_a, 0);
-	ft_push(first_b, first_a, 0);
-	ft_rotate(first_a, 0);
+	
 }
 
 void	ft_five_numbers(t_first *first_a, t_first *first_b)
@@ -102,47 +111,18 @@ void	ft_five_numbers(t_first *first_a, t_first *first_b)
 	ft_push(first_a, first_b, 1);
 	ft_push(first_a, first_b, 1);
 	ft_three_numbers(first_a, 0);
-	stack_a = first_a->first;
-	stack_b = first_b->first;
-	while (stack_a != NULL)
-	{
-		ft_index(first_a);
-		if (first_a->first->content < first_b->first->content)
-		{
-			if (stack_b->index <= 2)
-			{
-				while (first_a->first->content != ft_is_biggest(first_a))
-					ft_rotate(first_a, 0);
-			}
-			else
-			{
-				while (first_a->first->content != ft_is_biggest(first_a))
-					ft_reverse_rotate(first_a, 0);
-			}
-		}
-		else
-		{
-			if (stack_b->index <= 2)
-			{
-				while (first_a->first->content != ft_is_smallest(first_a))
-					ft_rotate(first_a, 0);
-			}
-			else
-			{
-				while (first_a->first->content != ft_is_smallest(first_a))
-					ft_reverse_rotate(first_a, 0);
-			}
-		}
-		stack_a = stack_a->next;
-	}
+	ft_push_back(first_a, first_b);
+	ft_push(first_b, first_a, 0);
+	ft_push_back(first_a, first_b);		
+	ft_push(first_b, first_a, 0);
 
 	// a refaire
-	if (first_a->first->content < first_b->first->content)
+	/*if (first_a->first->content < first_b->first->content)
 		ft_rotate(first_a, 0);
 	ft_push(first_b, first_a, 0);
 	if (first_a->first->content < first_b->first->content)
 		ft_rotate(first_a, 0);
 	ft_push(first_b, first_a, 0);
 	ft_rotate(first_a, 0);
-	ft_rotate(first_a, 0);
+	ft_rotate(first_a, 0);*/
 }
